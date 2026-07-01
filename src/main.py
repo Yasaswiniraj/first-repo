44652d2d9eaacbd3448b4b91a39dcac122e7a4e1
Yasaswini Raj <yasaswiniraj0@gmail.com>
2026-07-01 19:29:14 +0530

    Create Tkinter window

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 import tkinter as tk
 
+# Create the main window
 root = tk.Tk()
 
+# Window title
 root.title("Calculator")
+
+# Window size (Width x Height)
 root.geometry("320x450")
+
+# Prevent resizing
 root.resizable(False, False)
 
+# Keep the window open
 root.mainloop()
